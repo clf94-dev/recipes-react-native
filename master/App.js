73359bc0facdec5555e1f000d5e8b1d1bf474cbf
clf94-dev <clf94@hotmail.es>
2021-07-10 07:13:53 +0200

    fixed issue with axios get

--- master/App.js
+++ master/App.js
@@ -1,10 +1,11 @@
-import * as React from 'react';
+import  React, {useState, useEffect} from 'react';
 import { View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Ionicons from 'react-native-ionicons';
+import axios from 'axios';
 
 import HomeScreen from './screens/HomeScreen'
 import FilterScreen from './screens/FilterScreen'
@@ -28,7 +29,7 @@ function HomeTabs(){
     activeTintColor: 'orange',
     inactiveTintColor: 'white',
     /* showLabel: false,*/
-    style: { width: '100%', height:80,backgroundColor: 'gray',borderWidth:1,borderColor: 'gray' , borderRadius:'10px'}
+    style: { width: '100%', height:80,backgroundColor: 'gray',borderWidth:1,borderColor: 'gray', marginBottom:0 }
     }}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Filter" component={FilterScreen} />
--- master/screens/HomeScreen.js
+++ master/screens/HomeScreen.js
@@ -1,11 +1,40 @@
-import * as React from 'react';
-import {View, Text, Button} from 'react-native'
+import  React, {useState, useEffect} from 'react';
+import {View, Text, Button, Image} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import axios from 'axios';
 export default function HomeScreen ({navigation}){
+
+  const [info, setInfo] = useState([])
+  useEffect(() =>{
+    async function getRecipes(index){
+      let array= [];
+      while(index<11){
+      await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then(response=> {
+        console.log('response', response)
+array[index] = response.data.meals[0].strMealThumb
+        //setInfo([...info, response.data.meals[0].strMealThumb])
+        index++;
+      })
+      .catch(error=>{
+        console.log('error', error)
+      })
+    }
+    console.log('array', array)
+    setInfo(array)
+    }
+    let index = 0;
+    console.log('useEffect')
+    
+      console.log('index', index)
+     getRecipes(index)
+    
+ 
+  },[true])
     return(
-        <SafeAreaView>
+        <SafeAreaView edges={['right', 'bottom', 'left']}>
         <View>
+            <Image style={{width:'100%', height: 350, borderBottomLeftRadius:50,borderBottomRightRadius:50}} source={info[1]}/>
 <Text>
     Home
 </Text>
