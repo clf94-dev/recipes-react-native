bcf280b8f0f7fdea5312a51a3e5871d8d6a534b6
clf94-dev <clf94@hotmail.es>
2021-07-10 09:18:18 +0200

    added recipe image & name

--- master/screens/RecipeScreen.js
+++ master/screens/RecipeScreen.js
@@ -1,14 +1,16 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native'
+import {View, Text, Button, Image} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function RecipeScreen ({navigation}){
+export default function RecipeScreen ({route}){
+    data = route.params;
+console.log('data', data)
     return(
-        <SafeAreaView>
+        <SafeAreaView edges={['right', 'bottom', 'left']}>
         <View>
-<Text>
-  Recipe
-</Text>
+        <Image style={{width: 400, height: 500}}  source={{uri:`${data.data.strMealThumb}`}} />
+                     <Text  style={{ fontSize: 30,paddingTop: 20, paddingLeft:10, color: 'orange'}} >{data.data.strMeal} ({data.data.strArea})</Text>
+              <Text style={{ fontSize: 20,paddingTop: 20, paddingLeft:10, color: 'gray'}}>Ingredients:</Text>
 
         </View>
         </SafeAreaView>
