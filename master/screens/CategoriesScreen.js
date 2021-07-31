8b650844ab7b7f1d096f721ad18dc5393549cfc4
clf94-dev <clf94@hotmail.es>
2021-07-31 12:22:11 +0200

    connected CategoriesScreen with RecipeScreen

--- master/screens/CategoriesScreen.js
+++ master/screens/CategoriesScreen.js
@@ -1,32 +1,50 @@
 import axios from 'axios';
 import  React , {useState, useEffect, useRef} from 'react';
-import {View, Text, Button, Image, FlatList, TouchableOpacity} from 'react-native'
+import {View, Text, Button, Image, FlatList, TouchableOpacity, SliderComponent} from 'react-native'
 //import { FlatList , TouchableOpacity} from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function categoriesScreen ({navigation,route}){
     const categoryName = route.params.data;
     const [categoryList, setCategoryList] = useState([])
+    const [searchInfo, setSearchInfo] = useState({})
 
     async function getRecipesCategory(categoryName){
-        console.log('categoryName', categoryName)
         const params = {
 c: categoryName,
         }
         await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php',  {params})
         .then(response =>{
-            console.log('response', response)
             setCategoryList(response.data.meals)})
         .catch(error => console.log('error get category recipes', error))
        
     }
+    async function getInfoRecipe(name) {
+        let res=[]
+        const params={
+          s: name
+        }
+           await axios.get('https://www.themealdb.com/api/json/v1/1/search.php', {params})
+            .then(response =>
+             ( res = response.data.meals[0]))
+ 
+            .catch(error => console.log('error searchRecipeInfo', error))
+            setSearchInfo(res)
+
+            }
+
     useEffect(()=>{
         getRecipesCategory(categoryName)
+        setSearchInfo({})
     }, [true])
 
     useEffect(()=>{
-        console.log('categoryList', categoryList)
-    }, [categoryList])
+      
+    }, [categoryList, searchInfo])
+    useEffect(()=>{
+       if(searchInfo)
+        navigation.navigate('Recipe', {data:searchInfo}) 
+    },[searchInfo])
     const flatlistRef = useRef();
     return(
         <SafeAreaView edges={['right', 'bottom', 'left', 'top']}>
@@ -35,7 +53,11 @@ c: categoryName,
     {categoryName}
 </Text>
 <FlatList ref={flatlistRef} vertical showsVerticalScrollIndicator='false' /* style={{maxHeight:370}} */ data={categoryList} keyExtractor={item => item.idMeal} renderItem={({item}) => (
-             <TouchableOpacity onPress={() => navigation.navigate('Recipe', {data:item})}>
+             <TouchableOpacity onPress={async () => {
+                await getInfoRecipe(item.strMeal)
+            
+         
+                }}>
              <View >
                      <Image style={{width: 400, height: 400}}  source={{uri:`${item.strMealThumb}`}} />
                      <Text style={{position: 'absolute', top: 315, left:25, fontSize: 20, color: 'white'}}>{item.strMeal}</Text>
--- master/screens/RecipeScreen.js
+++ master/screens/RecipeScreen.js
@@ -1,5 +1,7 @@
-import  React, {useRef} from 'react';
+import axios from 'axios';
+import  React, {useEffect, useRef, useState} from 'react';
 import {View, Text, Button, Image, FlatList, ScrollView, Animated, Dimensions} from 'react-native'
+import { set } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import SlidingUpPanel from "rn-sliding-up-panel";
@@ -7,24 +9,29 @@ const { height } = Dimensions.get("window");
 
 
 export default function RecipeScreen ({route}){
+   
+       /*  const draggableRange={ top: height   +180 - 64 , bottom: 180 }
+       */
     
-        const draggableRange={ top: height /*  +180 - 64 */, bottom: 180 }
-      
+     /*  _draggedValue = new Animated.Value(180); */
+    const data = route.params;
     
-      _draggedValue = new Animated.Value(180);
-    data = route.params;
     let index = 0;
     let ingredientsArray= []
-    while(index<20){
-        ingredient = 'strIngredient' + parseInt(index+1 )
-        console.log('ingredient', ingredient)
-        amount = 'strMeasure' + parseInt(index+1)
+    
+    if(data) 
+   while(index<20){
+    ingredient = 'strIngredient' + parseInt(index+1 )
+    amount = 'strMeasure' + parseInt(index+1)
 if(data.data[ingredient] && data.data[amount]){ingredientsArray[index] = {ingredient: data.data[ingredient], amount:data.data[amount]}
-    index++;}else index=20;
+  index++;}
+else index=20;
+
 
 }
+ 
 
-const { top, bottom } = draggableRange;
+/* const { top, bottom } = draggableRange;
 
 const backgoundOpacity = this._draggedValue.interpolate({
   inputRange: [height - 48, height],
@@ -54,28 +61,31 @@ const textScale = this._draggedValue.interpolate({
   inputRange: [bottom, top],
   outputRange: [1, 0.7],
   extrapolate: "clamp"
-});
+}); */
     //const flatlistRef = useRef();
     return(
         <SafeAreaView edges={['right', 'bottom', 'left']} >
+          <View>
         <ScrollView>
-        <Image style={{width: 400, height: 500}}  source={{uri:`${data.data.strMealThumb}`}} />
+        
+          <Image style={{width: 400, height: 500}}  source={{uri:`${data.data.strMealThumb}`}} />
                      <Text  style={{ fontSize: 30,paddingTop: 20, paddingLeft:10, color: 'orange'}} >{data.data.strMeal} ({data.data.strArea})</Text>
-              <Text style={{ fontSize: 20,paddingTop: 20, paddingLeft:10, color: 'gray'}}>Ingredients:</Text>
+                    <Text style={{ fontSize: 20,paddingTop: 20, paddingLeft:10, color: 'gray'}}>Ingredients:</Text>
         
            { ingredientsArray.map(item => 
             <Text style={{ fontSize: 18,paddingTop: 20, paddingLeft:10, color: 'gray'}}>{item.amount} of <Text style={{color:'orange'}}>{item.ingredient}</Text> </Text>
-)} 
- <Text onPress={() => this._panel.show(360)}>Steps</Text>
- <SlidingUpPanel
+)}
+     
+{/* <Text onPress={() => this._panel.show(360)}>Steps</Text>
+  <SlidingUpPanel
           ref={c => (this._panel = c)}
           draggableRange={draggableRange}
           animatedValue={this._draggedValue}
           snappingPoints={[360]}
-          height={height /* + 180 */}
+          height={height + 180 }
           friction={0.5}
         >
-          <View /* style={styles.panel} */>
+          <View  style={styles.panel} >
             <Animated.View
               style={[
                // styles.iconBg,
@@ -85,7 +95,7 @@ const textScale = this._draggedValue.interpolate({
                 }
               ]}
             />
-            <View /* style={styles.panelHeader} */>
+            <View /* style={styles.panelHeader} >
               <Animated.View
                 style={{
                   transform: [
@@ -95,17 +105,18 @@ const textScale = this._draggedValue.interpolate({
                   ]
                 }}
               >
-                <Text /* style={styles.textHeader} */>Steps</Text>
+                <Text /* style={styles.textHeader} >Steps</Text>
               </Animated.View>
             </View>
-            <View /* style={styles.container} */>
+            <View /* style={styles.container} >
               <Text>1</Text>
               <Text>1</Text>
             </View>
           </View>
-        </SlidingUpPanel>
-     
+        </SlidingUpPanel> */}
+   
         </ScrollView>
+        </View>
         </SafeAreaView>
     )
 }
